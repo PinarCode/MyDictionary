4bcef6d8c957efb18c6053df069426ffd7d15c73
PINAR KAYA <kayapinar4706@gmail.com>
2021-02-25 15:02:09 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> cities = new MyDictionary<string, string>();
+            cities.Add("06", "Ankara");
+            cities.Add("47", "Mardin");
+            cities.Add("34", "İstanbul");
+            cities.Add("01", "Adana");
+            cities.ListItems();
+        }
+    }
+}
